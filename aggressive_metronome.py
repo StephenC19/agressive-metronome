2c4f15e27cd3c77777ee4480a9d432d8cae33a4a
Stephen Colfer <stephen.colfer@ucdconnect.ie>
2021-12-02 00:25:59 +0000

    control over darts

--- aggressive_metronome.py
+++ aggressive_metronome.py
@@ -1,44 +1,90 @@
+import os
 import mido
+import time
+import threading
 import RPi.GPIO as GPIO
 from pathlib import Path
-import time
-import os
+from playsound import playsound
+from foam_gun_control import *
+import subprocess
 
-SERVO_PIN_1 = 18
-SERVO_PIN_2 = 20
-TRIGGER_NOTE = 52
+RIGHT_SERVO_PIN = 2
+LEFT_SERVO_PIN = 17
+POWER_SERVO_PIN = 3
 
-# Metronome flags
-def on_beat():
-    Path('/tmp/beat.txt').touch()
+metronome_sound_file = "metronome_tick.mp3"
 
-def off_beat():
-    os.remove("/tmp/beat.txt")
+# Metronome
+def play_blip_sound():
+    playsound(metronome_sound_file)
 
-def start_counter(bpm, time_length=60):
+def calculate_accepted_times(bpm, duration):
+    current_time = time.time()
+    acceptable_times = []
+    count = bpm * (duration/60)
+    for i in range(count):
+        acceptable_times.append(current_time + (60/bpm))
+
+    return acceptable_times
+
+def in_time(hit_time, accepted_times, threshold):
+    shoot = True
+    print("Hit time: " + str(hit_time))
+    for i in accepted_times:
+        if abs(hit_time - i) < threshold:
+            print("in time - hit: " + str(hit_time) + ", accepted: " + str(i))
+            shoot = False
+            break
+        # elif i < hit_time:
+        #     accepted_times.remove(i)
+    return shoot
+
+def start_metronome(bpm, time_length):
     count = bpm * (time_length/60)
     for i in range(count):
-        on_beat()
+        sound_thread = threading.Thread(target=play_blip_sound, args=())
+        sound_thread.start()
         time.sleep(60/bpm)
-        off_beat()
 
+# Choose the correct MIDI device
+device_list = mido.get_input_names()
+device_choice = input("Select the midi device by inputting the number choice:\n" + str(device_list))
+midi_device = device_list[int(device_choice) - 1]
 
-speed = input("Enter the metronome speed in bpm")
-how_long = input("Enter how long you'd like the metronome to run (in seconds)")
-
-start_counter(speed, how_long)
 
 # Open MIDI device connection
 try:
-    midi_connection = mido.open_input(MIDI_UNIT)
+    # 'TD-17:TD-17 MIDI 1 20:0'
+    midi_connection = mido.open_input(midi_device)
 except:
     print("No MIDI device found. Please make sure a device is connected.")
     exit(1)
 
 
-for msg in midi_connection:
-    if msg.type == "note_on" and msg.note == TRIGGER_NOTE:
-        trigger_nerf_gun()
+# Setup Metronome
+bpm = input("Enter the metronome speed in bpm")
+duration = input("Enter how long you'd like the metronome to run (in seconds)")
+print("The motivating metronome will start once you hit the first note. \nGood Luck!")
+# 4 count in
 
+accepted_times = calculate_accepted_times(bpm, duration)
+print(accepted_times)
+threshold = 0.30
 
-120 = 0.5
+try:
+    time.sleep(2)
+    # Start playing sounds
+
+    # Wait for first hit
+    for msg in midi_connection:
+        start_metronome(bpm, duration)
+        break
+
+    # Start aggressive metronomemetronome
+    for msg in midi_connection:
+        current_time = time.time()
+        if not in_time(current_time, accepted_times, threshold):
+            shoot()
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
---
+++ foam_gun_control.py
@@ -0,0 +1,71 @@
+import RPi.GPIO as GPIO
+
+RIGHT_SERVO_PIN = 2
+LEFT_SERVO_PIN = 17
+POWER_SERVO_PIN = 3
+
+# Servo control
+def setup_servo(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    return GPIO.PWM(pin, 50)
+
+def set_angle(angle, pin, servo_control):
+    duty = angle / 18 + 2
+    GPIO.output(pin, True)
+    servo_control.ChangeDutyCycle(duty)
+    GPIO.output(pin, False)
+
+def safe_angle(servo_control, side):
+    left = 54
+    right = 160
+    if side == 'left':
+        set_angle(left, LEFT_SERVO_PIN, servo_control)
+    else:
+        set_angle(right, RIGHT_SERVO_PIN, servo_control)
+
+def trigger_angle(servo_control, side):
+    left = 140
+    right = 30
+    if side == 'left':
+        set_angle(left, LEFT_SERVO_PIN, servo_control)
+    else:
+        set_angle(right, RIGHT_SERVO_PIN, servo_control)
+
+def turn_on_power(servo_control):
+    set_angle(70, POWER_SERVO_PIN, servo_control)
+
+def turn_off_power(servo_control):
+    set_angle(90, POWER_SERVO_PIN, servo_control)
+
+def shoot():
+    RIGHT_SERVO_PIN = 2
+    LEFT_SERVO_PIN = 17
+    POWER_SERVO_PIN = 3
+
+    GPIO.setmode(GPIO.BCM)
+
+    # Script start
+    l = setup_servo(LEFT_SERVO_PIN)
+    r = setup_servo(RIGHT_SERVO_PIN)
+    m = setup_servo(POWER_SERVO_PIN)
+
+    l.start(5)
+    r.start(10.888)
+    m.start(7)
+
+    turn_on_power(m)
+
+    trigger_angle(l, 'left')
+    trigger_angle(r, 'right')
+    time.sleep(1)
+
+    turn_off_power(m)
+
+    safe_angle(l, 'left')
+    safe_angle(r, 'right')
+    time.sleep(1)
+
+    l.stop()
+    r.stop()
+    m.stop()
+    GPIO.cleanup()
